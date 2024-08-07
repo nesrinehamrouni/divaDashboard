a60e73c50e2c1f55732ba75a5f9029f956028477
nesrinehamrouni <nesrine.hamrouni@enicar.ucar.tn>
2024-08-07 12:41:59 +0100

    Added Laravel notification schedular

--- divaDashboard-master/app/Console/Commands/DispatchDailyNotifications.php
+++ divaDashboard-master/app/Console/Commands/DispatchDailyNotifications.php
@@ -6,11 +6,13 @@ use Illuminate\Console\Command;
 use App\Models\User;
 use App\Jobs\NotificationScheduleJob;
 use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\NotificationController;
 
 class DispatchDailyNotifications extends Command
 {
     protected $signature = 'notifications:dispatch-daily';
     protected $description = 'Dispatch daily notifications for all users';
+    
 
     public function handle()
     {
@@ -21,12 +23,18 @@ class DispatchDailyNotifications extends Command
         Log::info('Found ' . $users->count() . ' users with device keys');
 
         foreach ($users as $user) {
+            // Dispatch the job
             dispatch(new NotificationScheduleJob(
-                $user->nom,
-                $user->email,
+                "Daily Notification", // title
+                "This is your daily notification", // body
                 $user->device_key
             ));
-            Log::info("Dispatched notification for user: {$user->id}");
+            Log::info("Dispatched notification job for user: {$user->id}");
+
+            // Send the notification immediately
+
+            NotificationController::notify("Daily Notification", "This is your daily notification", $user->device_key);
+            Log::info("Sent immediate notification for user: {$user->id}");
         }
 
         Log::info('Completed daily notification dispatch');
--- divaDashboard-master/app/Console/Kernel.php
+++ divaDashboard-master/app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\DispatchDailyNotifications;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,9 +14,12 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
+    protected $commands = [
+        \App\Console\Commands\DispatchDailyNotifications::class,
+    ];
     protected function schedule(Schedule $schedule)
 {
-    $schedule->command('notifications:dispatch-daily')->dailyAt("11:15")
+    $schedule->command('notifications:dispatch-daily')->dailyAt("11:41")
     ->withoutOverlapping()->appendOutputTo(storage_path('logs/notification-dispatch.log')); ;
 }
     /**
