c9793ae9c59197a8f45704059c4da3ce40bc0b93
HattourWejden <wejden.hattour@eniso.u-sousse.tn>
2024-08-19 14:37:40 +0100

    added reglement backend with same ui updated

--- divaMobileNew-master/lib/pages/Menu/ReglementPage.dart
+++ divaMobileNew-master/lib/pages/Menu/ReglementPage.dart
@@ -1,58 +1,87 @@
 import 'package:divamobile/Api.dart';
+import 'package:divamobile/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
+import 'package:side_sheet/side_sheet.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+// Import the constants file
 
 class ReglementPage extends StatefulWidget {
   @override
   _ReglementPageState createState() => _ReglementPageState();
 }
-Future<String?> getStoredToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('auth_token');
-  }
-class _ReglementPageState extends State<ReglementPage> {
-Future<List<Map<String, dynamic>>> _fetchReglements() async {
-  final token = await getStoredToken(); // Implement this method to retrieve the stored token
-  final response = await http.get(
-    Uri.parse(BaseUrl.getReglements),
-    headers: {
-      'Authorization': 'Bearer $token',
-    },
-  );
 
-  if (response.statusCode == 200) {
-    // Parse the JSON data
-    List<dynamic> data = json.decode(response.body);
-    return data.map((item) {
-      return {
-        'num_piece': item['PINO'],
-        'client': item['TIERS'],
-        'htmt': double.tryParse(item['HTMT']) ?? 0.0,
-      };
-    }).toList();
-  } else {
-    print('Failed to load reglements: ${response.statusCode}');
-    print('Response body: ${response.body}');
-    throw Exception('Failed to load reglements');
-  }
+Future<String?> getStoredToken() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString('auth_token');
 }
+
+class _ReglementPageState extends State<ReglementPage> {
+  bool _isSideSheetOpen = false;
+
+  void _showSideSheet() async {
+    setState(() {
+      _isSideSheetOpen = true;
+    });
+
+    await SideSheet.right(
+      width: MediaQuery.of(context).size.width * 0.8,
+      body: _FilterWidget(),
+      context: context,
+    );
+
+    // When the side sheet is closed, set state to show FAB again
+    setState(() {
+      _isSideSheetOpen = false;
+    });
+  }
+
+  Future<List<Map<String, dynamic>>> _fetchReglements() async {
+    final token = await getStoredToken();
+    final response = await http.get(
+      Uri.parse(BaseUrl.getReglements),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      return data.map((item) {
+        return {
+          'num_piece': item['PINO'],
+          'client': item['TIERS'],
+          'htmt': double.tryParse(item['HTMT']) ?? 0.0,
+        };
+      }).toList();
+    } else {
+      print('Failed to load reglements: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      throw Exception('Failed to load reglements');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Consultation des règlements', style: TextStyle(color: Colors.white)),
         flexibleSpace: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topRight,
               end: Alignment.topLeft,
-              colors: <Color>[Colors.blue, Colors.green], // Replace with your colors
+              colors: <Color>[primaryColor, accentColor], // Use defined colors
             ),
           ),
+        ),
+        title: Text("Consultation Pièces Fournisseur",style: TextStyle(color: Colors.white , fontSize: 15.5) ,),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
         ),
       ),
       body: Container(
@@ -79,7 +108,7 @@ Future<List<Map<String, dynamic>>> _fetchReglements() async {
                       Container(
                         padding: EdgeInsets.symmetric(vertical: 12),
                         decoration: BoxDecoration(
-                          color: Colors.blue, // Replace with your color
+                          color: primaryColor, // Use defined color
                           borderRadius: BorderRadius.only(
                             topLeft: Radius.circular(12),
                             topRight: Radius.circular(12),
@@ -102,7 +131,14 @@ Future<List<Map<String, dynamic>>> _fetchReglements() async {
           },
         ),
       ),
-      
+      floatingActionButton: !_isSideSheetOpen
+          ? FloatingActionButton(
+              backgroundColor: kPrimaryColor, // Use defined color
+              child: Icon(Icons.filter_alt_outlined, color: Colors.white),
+              onPressed: _showSideSheet,
+            )
+          : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
     );
   }
 
@@ -126,8 +162,7 @@ Future<List<Map<String, dynamic>>> _fetchReglements() async {
         children: [
           _buildDataCell(reglement['num_piece'], flex: 2),
           _buildDataCell(reglement['client'], flex: 3),
-          _buildDataCell('${reglement['htmt'].toStringAsFixed(2)} ', flex: 2),
-         
+          _buildDataCell('${reglement['htmt'].toStringAsFixed(2)}', flex: 2),
         ],
       ),
     );
@@ -142,6 +177,41 @@ Future<List<Map<String, dynamic>>> _fetchReglements() async {
           text,
           style: TextStyle(color: Colors.black87),
           textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+
+  Widget _FilterWidget() {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text("Tiers"),
+              SizedBox(height: 5.h),
+              // Replace with your actual dropdown widget
+              DropdownButton<String>(
+                items: <String>['Option 1', 'Option 2'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (_) {},
+              ),
+              SizedBox(height: 15.h),
+              // More filter widgets here...
+              ElevatedButton(
+                onPressed: () {
+                  // Perform the search action
+                  Navigator.pop(context);
+                },
+                child: Text('Rechercher'),
+              ),
+            ],
+          ),
         ),
       ),
     );
